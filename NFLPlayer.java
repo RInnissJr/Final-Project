fdd7580ff8018501be3c24e4a023facc912af3d6
RInnissJr <roguesoul25@gmail.com>
2017-04-13 12:36:32 -0700

    Add files via upload

--- NFLPlayer.java
+++ NFLPlayer.java
@@ -160,7 +160,7 @@ public class NFLPlayer
 	double InsideOpponent40;
 	public void insideopponent40(double io40) 
 	{
-		InsideOpponent20 = io40;
+		InsideOpponent40 = io40;
 	}
 	double PlaysDefended;
 	public void playsDefended(double pdef) 
@@ -183,27 +183,125 @@ class PlayerManager
 	String playername;
 	int age;
 	double height;
+	String gender;
+	String position;//
+	int gamesPlayed;
+	int gamesStarted;
+	int uniformNumber;
+	double averageValue;
+	double passesComplete;
+	double passesAttempt;
+	double touchdownPasses;
+	double interceptionsThrown;
+	double sacked;
+	double fumbles;
+	double rating;
+	double yardsGained;
+	double yardsLostToSack;
+	double attempts;
+	double yards;
+	double longest;
+	double touchDowns;
+	double lost;
+	double totalTackles;
+	double sakes;
+	double saftey;
+	double passesDefended;
+	double interception;
+	double insideopponent20;
+	double insideopponent40;
+	double playsdefended;
+	double assists;
+	double comb;
+	
+	
 
-public void userInternalDatabase (PlayerManager info) {
+public void userInternalDatabase (PlayerManager info) 
+{
     this.playername = info.playername;
     this.age = info.age;
     this.height = info.height;
+    this.gender= info.gender;
+    this.position=info.position;
+    this.gamesPlayed=info.gamesPlayed;
+    this.gamesStarted=info.gamesStarted;
+    this.uniformNumber=info.uniformNumber;
+    this.averageValue=info.averageValue;
+    this.passesComplete=info.passesComplete;
+    this.passesAttempt=info.passesAttempt;
+    this.touchdownPasses=info.touchdownPasses;
+    this.interceptionsThrown=info.interceptionsThrown;
+    this.sacked=info.sacked;
+    this.fumbles=info.fumbles;
+    this.rating=info.rating;
+    this.yardsGained=info.yardsGained;
+    this.yardsLostToSack=info.yardsLostToSack;
+    this.attempts=info.attempts;
+    this.yards=info.yards;
+    this.longest=info.longest;
+    this.totalTackles=info.totalTackles;
+    this.sakes=info.sakes;
+    this.saftey=info.saftey;
+    this.passesDefended=info.passesDefended;
+    this.interception= info.interception;
+    this.insideopponent20=info.insideopponent20;
+    this.insideopponent40=info.insideopponent40;
+    this.playsdefended=info.playsdefended;
+    this.assists=info.assists;
+    this.comb=info.comb;
 }
-ArrayList <PlayerManager> InfoList = new ArrayList<PlayerManager> ();
 
-public void addUser(String i, int j, double k)
+ArrayList<PlayerManager> InfoList = new ArrayList<PlayerManager> ();
+
+public void addUser(String pn, int ag, double hei, String gen, String pos, int gp, int gs, int ufn,
+double av, double pc, double pa, double tdp, double ict, double sc, double fmb, double rt, double ydg,
+double ydlts, double att, double yds, double lng, double tt,double sks, double saf, double pssdef,
+double inter, double inso20, double inso40, double pd, double asis, double com)
 {
 	PlayerManager newUser = new PlayerManager();
-    newUser.playername = i;
-    newUser.age = j;
-    newUser.height = k;
+    newUser.playername = pn;
+    newUser.age = ag;
+    newUser.height = hei;
+    newUser.gender=gen;
+    newUser.position=pos;
+    newUser.gamesPlayed=gp;
+    newUser.gamesStarted=gs;
+    newUser.uniformNumber=ufn;
+    newUser.averageValue=av;
+    newUser.passesComplete=pc;
+    newUser.passesAttempt=pa;
+    newUser.touchdownPasses=tdp;
+    newUser.interceptionsThrown=ict;
+    newUser.sacked=sc;
+    newUser.fumbles=fmb;
+    newUser.rating=rt;
+    newUser.yardsGained=ydg;
+    newUser.yardsLostToSack=ydlts;
+    newUser.attempts=att;
+    newUser.yards=yds;
+    newUser.longest=lng;
+    newUser.totalTackles=tt;
+    newUser.sakes=sks;
+    newUser.saftey=saf;
+    newUser.passesDefended=pssdef;
+    newUser.interception= inter;
+    newUser.insideopponent20=inso20;
+    newUser.insideopponent40=inso40;
+    newUser.playsdefended=pd;
+    newUser.assists=asis;
+    newUser.comb=com;
     InfoList.add(newUser);
 }
-
+public ArrayList<PlayerManager> getUser()
+{
+	return InfoList;
+}
 public Object findUsername(String a)  
 {    
-    for (int i=0; i <InfoList.size(); i++) {
-        if (InfoList.get(i).playername.equals(a)){
+    for (int i=0; i <InfoList.size(); i++)
+    {
+        if (InfoList.get(i).playername.equals(a))
+        {
              return "This user already exists in our database.";
         }
     }
